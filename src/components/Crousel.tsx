c9f17d4ee8c2e8c53f3f02d066d001c3a26b5f5d
karan_syan <ksyan26@gmail.com>
2023-02-10 00:25:08 +0530

    crousel changed

--- src/components/Crousel.tsx
+++ src/components/Crousel.tsx
@@ -1,6 +1,6 @@
 // import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 import HorizontalRuleIcon from "@mui/icons-material/HorizontalRule";
-import { Box, createTheme, styled } from "@mui/material";
+import { Box, createTheme, styled, Typography } from "@mui/material";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import Carousel from "react-material-ui-carousel";
@@ -18,7 +18,6 @@ export default function Crousel({ item }: { item: IMovie[] }) {
     <Carousel
       autoPlay={true}
       duration={450}
-      // navButtonsAlwaysVisible
       IndicatorIcon={
         <HorizontalRuleIcon
           sx={{
@@ -43,8 +42,7 @@ export default function Crousel({ item }: { item: IMovie[] }) {
           title,
         } = item;
         return (
-          <div
-            className="flex w-full cursor-pointer"
+          <Root
             key={id}
             onClick={() => {
               if (user) {
@@ -56,100 +54,88 @@ export default function Crousel({ item }: { item: IMovie[] }) {
               }
             }}
           >
-            <div
-              className="flex-col z-0 justify-center w-1/2 text-sm relative font-extralight hidden md:flex md:px-2 xl:text-lg"
-              style={{ background: "#00040a", paddingLeft: "40px" }}
-            >
-              <h1 className="mb-1 text-base xl:text-xl font-extrabold">
-                {title ? title : name}
-              </h1>
-              <h1 className="mb-2 opacity-50 text-xm xl:text-base">
-                {release_date ? release_date : first_air_date}
-              </h1>
-              <h1 className="mb-1 line-clamp-3 w-11/12">{overview}</h1>
-            </div>
-            <div
-              className="w-1/5 z-10 absolute hidden h-full md:inline-block"
-              style={{
-                left: "39.85%",
-                backgroundImage:
-                  "linear-gradient(to right, #00040a, rgba(255,0,0,0))",
-              }}
-            ></div>
-            <img
+            <DetailContainer>
+              <Title>{title ? title : name}</Title>
+              <Date>{release_date ? release_date : first_air_date}</Date>
+              <Overview>{overview}</Overview>
+            </DetailContainer>
+            <ImgOverLay />
+            <Img
               src={`${MOVIE_DB_IMG_URL}${backdrop_path}`}
-              className="block w-full md:w-3/5 z-0"
+              draggable={false}
               alt="..."
             />
-          </div>
+          </Root>
         );
       })}
     </Carousel>
   );
 }
 
-const CrouselImgContainer = styled(Box)(({ theme }) => ({
+const Root = styled(Box)(({ theme }) => ({
   width: "100%",
   display: "flex",
-  position: "relative",
+  cursor: "pointer",
+}));
+const DetailContainer = styled(Box)(({ theme }) => ({
+  display: "none",
+  flexDirection: "column",
   justifyContent: "center",
-  height: "16vh",
-  alignItems: "center",
-  marginBottom: "10px",
+  width: "50%",
+  fontSize: "0.875rem",
+  fontWeight: "200",
+  background: theme.palette.secondary.main,
+  paddingLeft: "40px",
   [theme.breakpoints.up("md")]: {
-    height: "40vh",
+    display: "flex",
+    paddingRight: "0.5rem",
   },
   [theme.breakpoints.up("lg")]: {
-    height: "60vh",
+    fontSize: "1.125rem",
   },
-}));
-const Banner = styled("img")(({ theme }) => ({
-  minWidth: "60%",
-  height: "100%",
-  borderRadius: "30px",
-  objectFit: "cover",
-  position: "relative",
 }));
 const ImgOverLay = styled(Box)(({ theme }) => ({
   position: "absolute",
   width: "20%",
-  display: "inline-block",
+  display: "none",
   left: "40%",
   height: "100%",
   zIndex: 1,
-  backgroundImage: `linear-gradient(to right, ${theme.palette.background.default}, transparent)`,
+  backgroundImage: `linear-gradient(to right, ${theme.palette.secondary.main}, transparent)`,
+  [theme.breakpoints.up("md")]: {
+    display: "inline-block",
+  },
 }));
 const Img = styled("img")(({ theme }) => ({
-  borderRadius: "30px",
-  margin: "5px",
-  zIndex: "1",
-  display: "none",
+  display: "block",
+  width: "100%",
   [theme.breakpoints.up("md")]: {
-    display: "flex",
-    marginLeft: "56px",
+    width: "60%",
   },
-  height: "80%",
-  objectFit: "fill",
 }));
-const Container = styled(Box)(({ theme }) => ({
-  width: "60%",
-  height: "100%",
-  background: `${theme.palette.background.default}`,
-  // backgroundImage: `linear-gradient(to right, ${theme.palette.primary.main}, transparent)`,
+const Title = styled(Typography)(({ theme }) => ({
+  marginBottom: "0.25rem",
+  fontSize: "1rem",
+  fontWeight: "800",
+  [theme.breakpoints.up("lg")]: {
+    fontSize: "1.25rem",
+  },
 }));
-
-// sx={{
-//   display: "flex",
-//   alignItems: "center",
-//   flexDirection: "column",
-//   width: "100%",
-//   borderRadius: "30px",
-//   marginBottom: "12px",
-//   height: "19vh",
-//   [theme.breakpoints.up("md")]: {
-//     height: "43vh",
-//   },
-//   [theme.breakpoints.up("lg")]: {
-//     height: "63vh",
-//   },
-// }}
+const Date = styled(Typography)(({ theme }) => ({
+  marginBottom: "0.5rem",
+  opacity: "0.5",
+  fontSize: "0.75rem",
+  fontWeight: "800",
+  [theme.breakpoints.up("lg")]: {
+    fontSize: "1rem",
+  },
+}));
+const Overview = styled(Typography)(({ theme }) => ({
+  opacity: "0.5",
+  marginBottom: "0.25rem",
+  width: "91%",
+  overflow: "hidden",
+  display: "-webkit-box",
+  "-webkit-box-orient": "vertical",
+  "-webkit-line-clamp": "3",
+}));
