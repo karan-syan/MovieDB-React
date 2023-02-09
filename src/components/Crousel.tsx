48b12a2bcc0675a70e6c13db3131f2cea538dfe6
karan_syan <ksyan26@gmail.com>
2023-02-09 23:45:06 +0530

    commit on feb 9

--- src/components/Crousel.tsx
+++ src/components/Crousel.tsx
@@ -1,8 +1,11 @@
+// import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+import HorizontalRuleIcon from "@mui/icons-material/HorizontalRule";
+import { Box, createTheme, styled } from "@mui/material";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import Carousel from "react-material-ui-carousel";
 import { useNavigate } from "react-router-dom";
 import { firebaseConfig } from "../firebaseConfig";
-import { IMovie_distructing } from "../utils/ApiDistruct";
 import { IMovie } from "../utils/type";
 import { MOVIE_DB_IMG_URL } from "../utils/url";
 
@@ -10,122 +13,143 @@ export default function Crousel({ item }: { item: IMovie[] }) {
   const navigate = useNavigate();
   const app = initializeApp(firebaseConfig);
   const user = getAuth(app).currentUser;
+  const theme = createTheme();
   return (
-    <div className="flex justify-center xl:mt-3 mb-8 drop-shadow-2xl">
-      <div
-        id="carouselExampleCaptions"
-        className="carousel slide relative"
-        data-bs-ride="carousel"
-      >
-        <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4 z-20">
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="0"
-            className="active"
-            aria-current="true"
-            aria-label="Slide 1"
-          />
-          {item.map((val, index) => {
-            if (index > 0 && index <= 10) {
-              return (
-                <button
-                  key={index}
-                  type="button"
-                  data-bs-target="#carouselExampleCaptions"
-                  data-bs-slide-to={`${index}`}
-                  aria-label={`Slide ${index + 1}`}
-                />
-              );
-            }
-          })}
-        </div>
-        <div className="carousel-inner relative w-full overflow-hidden">
-          {item.map((val, index) => {
-            const {
-              I_name,
-              backdrop_path,
-              first_air_date,
-              id,
-              overview,
-              title,
-              release_date,
-            } = IMovie_distructing(val);
-
-            if (index <= 10) {
-              return (
-                <div
-                  className={`carousel-item relative float-left w-full  ${
-                    index === 0 ? "active" : ""
-                  }`}
-                  key={index}
-                  onClick={() => {
-                    if (user) {
-                      I_name
-                        ? navigate(`/tv/details/${id}`)
-                        : navigate(`/movie/details/${id}`);
-                    } else {
-                      navigate("/signin");
-                    }
-                  }}
-                >
-                  <div className="flex w-full cursor-pointer">
-                    <div
-                      className="flex-col z-0 justify-center w-1/2 text-sm relative font-extralight hidden md:flex md:px-2 xl:text-lg"
-                      style={{ background: "#00040a", paddingLeft: "40px" }}
-                    >
-                      <h1 className="mb-1 text-base xl:text-xl font-extrabold">
-                        {title ? title : I_name}
-                      </h1>
-                      <h1 className="mb-2 opacity-50 text-xm xl:text-base">
-                        {release_date ? release_date : first_air_date}
-                      </h1>
-                      <h1 className="mb-1 line-clamp-3 w-11/12">{overview}</h1>
-                    </div>
-                    <div
-                      className="w-1/5 z-10 absolute hidden h-full md:inline-block"
-                      style={{
-                        left: "39.85%",
-                        backgroundImage:
-                          "linear-gradient(to right, #00040a, rgba(255,0,0,0))",
-                      }}
-                    ></div>
-                    <img
-                      src={`${MOVIE_DB_IMG_URL}${backdrop_path}`}
-                      className="block w-full md:w-3/5 z-0"
-                      alt="..."
-                    />
-                  </div>
-                </div>
-              );
-            }
-          })}
-        </div>
-        <button
-          className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon inline-block bg-no-repeat"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon inline-block bg-no-repeat"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
-    </div>
+    <Carousel
+      autoPlay={true}
+      duration={450}
+      // navButtonsAlwaysVisible
+      IndicatorIcon={
+        <HorizontalRuleIcon
+          sx={{
+            fontSize: "1.3rem",
+            [theme.breakpoints.up("sm")]: {
+              fontSize: "2rem",
+            },
+          }}
+        />
+      }
+      animation={"slide"}
+      stopAutoPlayOnHover
+    >
+      {item.slice(0, 13).map((item, i) => {
+        const {
+          release_date,
+          backdrop_path,
+          id,
+          overview,
+          first_air_date,
+          name,
+          title,
+        } = item;
+        return (
+          <div
+            className="flex w-full cursor-pointer"
+            key={id}
+            onClick={() => {
+              if (user) {
+                name
+                  ? navigate(`/tv/details/${id}`)
+                  : navigate(`/movie/details/${id}`);
+              } else {
+                navigate("/signin");
+              }
+            }}
+          >
+            <div
+              className="flex-col z-0 justify-center w-1/2 text-sm relative font-extralight hidden md:flex md:px-2 xl:text-lg"
+              style={{ background: "#00040a", paddingLeft: "40px" }}
+            >
+              <h1 className="mb-1 text-base xl:text-xl font-extrabold">
+                {title ? title : name}
+              </h1>
+              <h1 className="mb-2 opacity-50 text-xm xl:text-base">
+                {release_date ? release_date : first_air_date}
+              </h1>
+              <h1 className="mb-1 line-clamp-3 w-11/12">{overview}</h1>
+            </div>
+            <div
+              className="w-1/5 z-10 absolute hidden h-full md:inline-block"
+              style={{
+                left: "39.85%",
+                backgroundImage:
+                  "linear-gradient(to right, #00040a, rgba(255,0,0,0))",
+              }}
+            ></div>
+            <img
+              src={`${MOVIE_DB_IMG_URL}${backdrop_path}`}
+              className="block w-full md:w-3/5 z-0"
+              alt="..."
+            />
+          </div>
+        );
+      })}
+    </Carousel>
   );
 }
+
+const CrouselImgContainer = styled(Box)(({ theme }) => ({
+  width: "100%",
+  display: "flex",
+  position: "relative",
+  justifyContent: "center",
+  height: "16vh",
+  alignItems: "center",
+  marginBottom: "10px",
+  [theme.breakpoints.up("md")]: {
+    height: "40vh",
+  },
+  [theme.breakpoints.up("lg")]: {
+    height: "60vh",
+  },
+}));
+const Banner = styled("img")(({ theme }) => ({
+  minWidth: "60%",
+  height: "100%",
+  borderRadius: "30px",
+  objectFit: "cover",
+  position: "relative",
+}));
+const ImgOverLay = styled(Box)(({ theme }) => ({
+  position: "absolute",
+  width: "20%",
+  display: "inline-block",
+  left: "40%",
+  height: "100%",
+  zIndex: 1,
+  backgroundImage: `linear-gradient(to right, ${theme.palette.background.default}, transparent)`,
+}));
+const Img = styled("img")(({ theme }) => ({
+  borderRadius: "30px",
+  margin: "5px",
+  zIndex: "1",
+  display: "none",
+  [theme.breakpoints.up("md")]: {
+    display: "flex",
+    marginLeft: "56px",
+  },
+  height: "80%",
+  objectFit: "fill",
+}));
+const Container = styled(Box)(({ theme }) => ({
+  width: "60%",
+  height: "100%",
+  background: `${theme.palette.background.default}`,
+  // backgroundImage: `linear-gradient(to right, ${theme.palette.primary.main}, transparent)`,
+}));
+
+// sx={{
+//   display: "flex",
+//   alignItems: "center",
+//   flexDirection: "column",
+//   width: "100%",
+//   borderRadius: "30px",
+//   marginBottom: "12px",
+//   height: "19vh",
+//   [theme.breakpoints.up("md")]: {
+//     height: "43vh",
+//   },
+//   [theme.breakpoints.up("lg")]: {
+//     height: "63vh",
+//   },
+// }}
